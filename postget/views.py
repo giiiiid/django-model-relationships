a10b73b57c4d6999fb31d9e3e53dd4fb29c0f79e
Giiid <gideonfummey1@gmail.com>
2023-11-08 23:58:54 +0100

    Querying data

--- postget/views.py
+++ postget/views.py
@@ -1,1 +1,22 @@
 from django.shortcuts import render, HttpResponse
+from .models import *
+from django.db.models import Q
+
+def orders(request):
+    # jollof = []
+    # for jollof_orders in Order.objects.all():
+    #     if 'ollof'.casefold() in jollof_orders.food.name:
+    #         jollof.append(jollof_orders)
+    
+    # for i in jollof:
+    #     context = {'i':i}
+
+    orders = Order.objects.first()
+    personName = orders.customer_name.name
+    personFood = orders.food.name
+
+    customer = Customer.objects.first()
+    ordered_items = customer.order_set.all()
+
+    context = {'personFood':personFood, 'personName':personName, 'ordered_items':ordered_items}
+    return render(request, 'orders.html', context)
