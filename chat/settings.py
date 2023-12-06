acecce2ef1e0e8ab6373c09e30192c817a537f34
Giiid <gideonfummey1@gmail.com>
2023-12-06 11:26:19 +0100

    todo model

--- chat/settings.py
+++ chat/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'postget',
-    'todo'
+    'todo',
     'rest_framework'
 ]
 
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Todo(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField()
+    is_complete = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
