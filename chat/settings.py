83843f21d521069ebba6b0d651cc500b94ca8d7c
Giiid <gideonfummey1@gmail.com>
2023-12-06 11:14:19 +0100

    minor changes

--- chat/settings.py
+++ chat/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'postget', 
+    'postget',
+    'todo'
     'rest_framework'
 ]
 
--- chat/urls.py
+++ chat/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('postget/', include('postget.urls')),
+    path('todo/', include('todo.urls')),
 ]
