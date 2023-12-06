27340ee41c37e2125719e501458c5b00a2ecdd3b
Giiid <gideonfummey1@gmail.com>
2023-12-07 00:12:56 +0100

    added api routing to main url routes

--- postget/api/urls.py
+++ postget/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import OrderAPI
 
 urlpatterns = [
-    
+    path('order', OrderAPI.as_view(), name="order_api")
 ]
--- postget/urls.py
+++ postget/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+# from api import views
+# from api import urls
 
 urlpatterns=[
     path('orders', views.orders, name='orders'),
     path('customers/<str:name>', views.customers, name='customers'),
     path('foods', views.foods, name='foods'), 
     path('create_order/<str:name>', views.create_order, name='create_order'),
-    # path('api', views.api, name='api')
+    path('api/', include('postget.api.urls'))
 ]
