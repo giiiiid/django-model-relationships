3787757c2c5e3ba1ccf3e663668749a5b192451c
Giiid <gideonfummey1@gmail.com>
2023-12-06 13:17:06 +0100

    APIView and RetrieveUpdateDestroyAPIView

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CreateTodo, ListTodo
+from .views import TodoAPI, TodoDetail #ListTodo, create_todo, list_todo #CreateTodo
 
 urlpatterns = [
-    path('create', CreateTodo.as_view(), name='create'),
-    path('list', ListTodo.as_view(), name='list')
+    path('todo', TodoAPI.as_view(), name='todo'),
+    path('tdetail/<str:name>', TodoDetail.as_view(), name='tdetail'),
+    # path('createtodo', create_todo, name='createtodo'),
+    # path('list', ListTodo.as_view(), name='list'), 
+    # path('listtodo', list_todo, name='listtodo')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,31 +1,70 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, GenericAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
 from .serializers import TodoSerializer
 from .models import Todo
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import response, status
+from rest_framework import response, status, decorators
 # Create your views here.
 
-class CreateTodo(CreateAPIView):
+# class CreateTodo(CreateAPIView):
+#     serializer_class = TodoSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     # def perform_create(self, serializer):
+#     #     return serializer.save(owner=self.request.user)
+#     #     return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+#     def post(self, request):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @decorators.api_view(["POST"])
+# def create_todo(request):
+#     serializer = TodoSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save(owner=request.user)
+#         return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class ListTodo(ListAPIView):
+#     serializer_class = TodoSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         queryset = Todo.objects.filter(owner=self.request.user)
+#         return queryset
+
+# @decorators.api_view(["GET"])
+# def list_todo(request):
+#     queryset_owner = Todo.objects.filter(owner=request.user)
+#     serializer = TodoSerializer(queryset_owner, many=True)
+#     return response.Response(serializer.data, status=status.HTTP_200_OK)
+
+class TodoAPI(APIView):
     serializer_class = TodoSerializer
     permission_classes = [IsAuthenticated]
 
-    # def perform_create(self, serializer):
-    #     return serializer.save(owner=self.request.user)
-    #     return response.Response(serializer.data, status=status.HTTP_201_CREATED)
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
-        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request):
+    #     serializer = self.serializer_class(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # @decorators.api_view(["GET"])
+    def get(self, request):
+        queryset = Todo.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return response.Response(serializer.data)
 
-
-class ListTodo(ListAPIView):
+class TodoDetail(RetrieveUpdateDestroyAPIView):
     serializer_class = TodoSerializer
     permission_classes = [IsAuthenticated]
+    lookup_field = "name"
 
     def get_queryset(self):
         queryset = Todo.objects.filter(owner=self.request.user)
         return queryset
-
